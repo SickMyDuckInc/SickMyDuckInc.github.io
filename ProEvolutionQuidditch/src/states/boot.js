273aa209fbbe620d3408dc0eea37d3fd9b8db606
jetese <jetese2@gmail.com>
2018-10-23 16:12:25 +0200

    Prueba preload

--- ProEvolutionQuidditch/src/states/boot.js
+++ ProEvolutionQuidditch/src/states/boot.js
@@ -1,17 +1,17 @@
 var ProEvolutionQuidditch = {}
 
 ProEvolutionQuidditch.bootState = function(game) {
-
+    
 }
 
 ProEvolutionQuidditch.bootState.prototype = {
 
     preload: function() {
-        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
     },
 
     create: function() {
-
+        this.state.start('preloadState');
     },
 
     update: function() {
