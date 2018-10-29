d9168d6ae7f11e77f9098cf0c7f5f489d2c439fd
sergio_sanchez <sergete86@gmail.com>
2018-10-29 18:15:02 +0100

    Spacebar añadida a introState

--- ProEvolutionQuidditch/src/states/intro.js
+++ ProEvolutionQuidditch/src/states/intro.js
@@ -7,22 +7,23 @@ ProEvolutionQuidditch.introState.prototype = {
     preload: function() {
         game.add.image(0,0,'background2');
 
-        //var TmpImg = game.cache.getImage('referee');
-        //game.add.sprite(game.world.centerX - TmpImg.width/2.0,game.world.centerY - TmpImg.height/2.0,'referee');
         game.add.sprite(game.world.centerX/1.5,game.world.centerY/5, 'referee');
         myimage = game.add.sprite(game.world.centerX/5,game.world.centerY+120, 'clean');
         myimage.scale.setTo(0.43,0.25);
-        //game.add.sprite(game.world.centerX,game.world.centerY/5, 'clean');
+
+        keypress = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        keypress.onDown.add(this.level, this);
     },
 
     create: function() {
-        setTimeout(function(){
-            console.log("LLego al siguiente estado");
-             game.state.start('levelState');
-             }, 3000);
+
     },
 
     update: function() {
 
+    },
+
+    level: function(){
+        this.state.start('levelState');
     }
 }
