49fac60af7ac2ed04748c4ac5af08f0a1170df7b
pepesegade <pepest97@gmail.com>
2018-10-30 16:42:09 +0100

    Juego de meatball

--- MeatballRain/src/index.js
+++ MeatballRain/src/index.js
@@ -1,5 +1,5 @@
 game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv')
   
-game.state.add('levelState', ProEvolutionQuidditch.levelState)
+game.state.add('levelState', MeatballRain.levelState)
   
 game.state.start('levelState')
