b689050fdc9db43026c60a60eef4251228e2e323
Alberto Blanco <alberto.blanco@spikatech.com>
2018-11-04 20:02:58 +0100

    Cuadricula responsive y click detectando en que casilla

--- GateCrasher/js/gameController.js
+++ GateCrasher/js/gameController.js
@@ -8,25 +8,65 @@ function startGame() {
     myGamePiece = new component(30, 30, "red", 10, 120);
     myGamePiece.gravity = 0.05;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
-    myGameArea.start();
 }
 var context;
+
 var myGameArea = {
-    canvas : $("#myCanvas"),
+    canvas : document.getElementById("myCanvas"),
     start : function() {
+        this.canvas = document.getElementById("myCanvas");
         this.context = this.canvas.getContext("2d");
         this.frameNo = 0;
     },
     clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        console.log("Cleared canvas");
+        this.context.clearRect(0, 0, this.canvas.scrollWidth, this.canvas.scrollHeight);
+    },
+    getWidth : function(){
+        return this.canvas.scrollWidth;
+    },
+    getHeight : function(){
+        return this.canvas.scrollHeight;
+    },
+    getDrawWidth : function(){
+        return this.canvas.width;
+    },
+    getDrawHeight : function(){
+        return this.canvas.height;
+    },
+    drawLine : function(fromX, fromY, toX, toY){
+        this.context.moveTo(fromX, fromY);
+        this.context.lineTo(toX, toY);
+        this.context.stroke();
+    },
+    setColor : function(color){
+        this.context.strokeStyle = color;
+    },
+    getPos : function(posX, posY){
+        var rect = this.canvas.getBoundingClientRect();
+        var realX = posX - rect.left;
+        var realY = posY - rect.top;
+        var pos = {x:realX, y:realY};
+        //console.log("posX: " + realX + " posY: " + realY);
+        return pos;
     }
 }
+
+var lManager;
+
+
 function resize(){    
     $("#myCanvas").outerHeight($(window).height()-$("#myCanvas").offset().top- Math.abs($("#myCanvas").outerHeight(true) - $("#myCanvas").outerHeight() -10));
+    lManager.drawMap();
 }
 $(document).ready(function(){
+    lManager = new levelManager(4, 6, myGameArea);
     resize();
     $(window).on("resize", function(){                      
         resize();
+    });
+
+    $("#myCanvas").on('click', function(e){
+        lManager.manageClick(e.clientX, e.clientY);
     });
 });
--- GateCrasher/js/levelManager.js
+++ GateCrasher/js/levelManager.js
@@ -5,3 +5,56 @@ Tiene tres matrices:
 - Pathfinding: Es la matriz que se utilizará para el pathfinding de los personajes (tiene las recompensas de cada casilla)
 - Matriz de objetos: Determina en que puntos se pueden colocar los objetos. El jugador la irá rellenando
 */
+
+var GRAY_COLOR = "#e6e6e6";
+var RED_COLOR = "#ff0000";
+var BLUE_COLOR = "#0000ff";
+
+function levelManager(rows, cols, canvas){
+    this.numRows = rows;
+    this.numCols = cols;
+    this.canvas = canvas;    
+
+    this.canvas.start();
+
+    this.intervalHeight = this.canvas.getHeight() / this.numRows;
+    this.intervalWidth = this.canvas.getWidth() / this.numCols;
+
+
+    console.log("Created levelManager with " + rows + " rows and " + cols + " cols");
+}
+
+levelManager.prototype.drawMap = function(){
+    this.canvas.clear();
+    this.intervalHeight = this.canvas.getHeight() / this.numRows;
+    this.intervalWidth = this.canvas.getWidth() / this.numCols;
+
+    var drawHeight = this.canvas.getDrawHeight() /this.numRows;
+    var drawWidth = this.canvas.getDrawWidth() / this.numCols;
+
+    this.canvas.setColor(GRAY_COLOR);
+
+    for(var i = 1; i<this.numRows; i++){
+        this.canvas.drawLine(0, i*drawHeight, this.canvas.getDrawWidth(), i*drawHeight);
+    }
+
+    for(var j = 1; j<this.numCols; j++){
+        this.canvas.drawLine(j*drawWidth, 0, j*drawWidth, this.canvas.getDrawHeight());
+    }
+
+    //this.canvas.clear();
+    console.log("Dibujando mapa");
+}
+
+
+levelManager.prototype.manageClick = function(posX, posY){
+    var canvasPos = this.canvas.getPos(posX, posY);
+
+    var casillaY = Math.floor(canvasPos.x / this.intervalWidth);
+    var casillaX = Math.floor(canvasPos.y / this.intervalHeight);
+
+    var casilla = {x:casillaX, y:casillaY};
+
+    console.log("X: " + casillaX + ", Y: " + casillaY);
+
+}
