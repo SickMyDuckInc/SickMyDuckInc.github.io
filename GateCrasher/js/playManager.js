8702b24ffdd999f39bb86c6f59e6b72888098d9d
Alberto Blanco <alberto.blanco@spikatech.com>
2018-12-03 18:39:47 +0100

    Empezado array para el playManager

--- GateCrasher/js/playManager.js
+++ GateCrasher/js/playManager.js
@@ -18,47 +18,70 @@ row: número de filas
 //Parámetro que determina el tiempo que tarda un personaje en avanzar a la siguiente casilla del mapa
 var TIME_SQUARE =  1;
 
+function playManager(actions, levelManager, allEnemies, character, canvas){
+
+    this.actions = actions;
+    this.levelManager = levelManager;
+    this.allEnemies = allEnemies;
+    this.character = character;
+    this.canvas = canvas;
+
+
+    this.updateInterval = setInterval(() => this.update(), 100);
+
+}
+
+playManager.prototype.update = function(){
+    this.canvas.clear();    
+
+    for(var element in this.allEnemies){
+        this.allEnemies.sprite.draw();
+    }
+
+    this.character.sprite.draw();
+}
+
 //Constructior de la clase, 
-function playManager(matrixMovement,listAction, listCharacters, myGameArea, col, rows){
-    this.matrixMovement = matrixMovement;
-    this.listAction = listAction;
-    this.listCharacters = listCharacters;
-    this.canvas = myGameArea;
-    this.col = col;
-    this.rows = rows;
+// function playManager(matrixMovement,listAction, listCharacters, myGameArea, col, rows){
+//     this.matrixMovement = matrixMovement;
+//     this.listAction = listAction;
+//     this.listCharacters = listCharacters;
+//     this.canvas = myGameArea;
+//     this.col = col;
+//     this.rows = rows;
 
-    //Calculamos el espacio de canvas que ocupa cada "rectangulo" del mapa
-    this.spaceX = this.canvas.getWidth() / this.numCols;
-    this.spaceY = this.canvas.getHeight() / this.numRows;
-    //Calculamos la velocidad en la que los personajes van a pasar de una casilla a la siguiente
-    this.velX = sizeSquareX / TIME_SQUARE;
-    this.velY = sizeSquareY / TIME_SQUARE;
-}
+//     //Calculamos el espacio de canvas que ocupa cada "rectangulo" del mapa
+//     this.spaceX = this.canvas.getWidth() / this.numCols;
+//     this.spaceY = this.canvas.getHeight() / this.numRows;
+//     //Calculamos la velocidad en la que los personajes van a pasar de una casilla a la siguiente
+//     this.velX = sizeSquareX / TIME_SQUARE;
+//     this.velY = sizeSquareY / TIME_SQUARE;
+// }
 
-//Función que recorre la lista de acciones a realizar(personajes.action()) en un tiempo t
-playManager.prototype.executeAction = function(time){
-    for(var i; i<this.listAction.length; i++){
-        this.listAction[i][time].action();
-    }
-}
+// //Función que recorre la lista de acciones a realizar(personajes.action()) en un tiempo t
+// playManager.prototype.executeAction = function(time){
+//     for(var i; i<this.listAction.length; i++){
+//         this.listAction[i][time].action();
+//     }
+// }
 
-//Función que mueve a los personaje de una casilla a la siguiente en un tiempo t
-playManager.prototype.executeMovement = function(time){
-    for (var i = 0; i<this.matrixMovement.length;i++){
-        this.matrixMovement[i][time].character.move(this.matrixMovement[i][time].x,this.matrixMovement[i][time].y,this.velX,this.velY);
-    }
-}
+// //Función que mueve a los personaje de una casilla a la siguiente en un tiempo t
+// playManager.prototype.executeMovement = function(time){
+//     for (var i = 0; i<this.matrixMovement.length;i++){
+//         this.matrixMovement[i][time].character.move(this.matrixMovement[i][time].x,this.matrixMovement[i][time].y,this.velX,this.velY);
+//     }
+// }
 
-//resize del canvas
-playManager.prototype.resize = function(){
-    this.spaceX = this.canvas.getWidth() / this.numCols;
-    this.spaceY = this.canvas.getHeight() / this.numRows;
-    this.velX = sizeSquareX / TIME_SQUARE;
-    this.velY = sizeSquareY / TIME_SQUARE;
-}
+// //resize del canvas
+// playManager.prototype.resize = function(){
+//     this.spaceX = this.canvas.getWidth() / this.numCols;
+//     this.spaceY = this.canvas.getHeight() / this.numRows;
+//     this.velX = sizeSquareX / TIME_SQUARE;
+//     this.velY = sizeSquareY / TIME_SQUARE;
+// }
 
-//Función que ejecuta las acciones y el movimiento de los personajes
-playerManager.prototype.startDraw = function(){
-    setInterval(executeMovement, 1/30);
-    nextStage(executeAction,TIME_SQUARE);
-}
+// //Función que ejecuta las acciones y el movimiento de los personajes
+// playerManager.prototype.startDraw = function(){
+//     setInterval(executeMovement, 1/30);
+//     nextStage(executeAction,TIME_SQUARE);
+// }
