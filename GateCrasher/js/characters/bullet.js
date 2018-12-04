5bb14ec582be7a7eadf5e2845de212fe835aa2ef
jetese <jetese2@gmail.com>
2018-12-04 11:39:08 +0100

    bala

--- GateCrasher/js/characters/bullet.js
+++ GateCrasher/js/characters/bullet.js
@@ -7,8 +7,9 @@ function bullet(direction, type, col, rows, vel, canvas,character){
     this.canvas = canvas;
     this.collisioned = false;
     this.character = character;
-    this.bulletSprite = new sprite(this.canvas.getContext(), "res/goodies/bullet.png", 100, 100, 50, 100);
+    this.bulletSprite = new sprite(this.canvas.getContext(), "res/goodies/bullet.png", 100, 100,rows*canvas.getDrawWidth(),col*canvas.getDrawHeight());
     this.bulletSprite.addAnimation("move",  "res/goodies/bullet_anim.png", 2, 200, 200);
+    this.bulletSprite.setSpeed(5);
     this.bulletSprite.playAnimation("move");
 
     this.updateInterval = setInterval(() => this.update(), 100);
