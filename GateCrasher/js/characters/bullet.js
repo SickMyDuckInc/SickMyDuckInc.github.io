7f2a09e87d88120fc65e77bca4d378b027672bec
Alberto Blanco <alberto.blanco@spikatech.com>
2018-12-04 10:58:56 +0100

    Cositas para las balas

--- GateCrasher/js/characters/bullet.js
+++ GateCrasher/js/characters/bullet.js
@@ -5,6 +5,7 @@ function bullet(direction, type, col, rows, vel, canvas){
     this.rows = rows;
     this.vel = vel;
     this.canvas = canvas;
+    this.collisioned = false;
     this.bulletSprite = new sprite(this.canvas.getContext(), "res/goodies/bullet.png", 100, 100, 50, 100);
     this.bulletSprite.addAnimation("move",  "res/goodies/bullet_anim.png", 2, 200, 200);
     this.bulletSprite.playAnimation("move");
