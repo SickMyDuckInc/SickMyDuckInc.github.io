fd6b4bee016a77a7b15e7aa54663ba107f01d1b8
jetese <jetese2@gmail.com>
2018-12-28 13:25:40 +0100

    Menu con botones que navegan entre escenas
    Controlador de personaje

---
+++ BancoCrasher/Balaclava/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartScreen()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void ConfigScreen()
+    {
+        SceneManager.LoadScene("Configuration");
+    }
+
+    public void CreditsScreen()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
