e70da818b547c38a227e2f56994998ae6b71ef28
sergio_sanchez <sergete86@gmail.com>
2019-01-01 23:38:23 +0100

    Camara prefab, vision y rutas enemigos

---
+++ BancoCrasher/Balaclava/Assets/Scripts/Waypoint/WaypointsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointsManager : MonoBehaviour
+{
+    public static WaypointsManager wp = null;
+
+    [Header("Scene Waypoints")]
+    public List<Transform> waypoints;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        wp = this;
+    }
+
+    public List<Transform> GetSceneWaypoints()
+    {
+        return waypoints;
+    }
+}
