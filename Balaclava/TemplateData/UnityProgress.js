adb0cef84417175c8aa7ab0b6089b11288627fdd
Alberto Blanco <alberto.blanco@spikatech.com>
2019-01-10 23:53:29 +0100

    Cosas del manual

--- Balaclava/TemplateData/UnityProgress.js
+++ Balaclava/TemplateData/UnityProgress.js
@@ -21,4 +21,5 @@ function UnityProgress(gameInstance, progress) {
   gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
   if (progress == 1)
     gameInstance.logo.style.display = gameInstance.progress.style.display = "none";
+    gameInstance.SetFullscreen(1);
 }
